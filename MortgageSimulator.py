0ceab8f754cec21840831022b030eba3a90017f7
xuxu <xu.xu.zhao@gmail.com>
2022-12-02 18:48:42 +1100

    delete plot

--- MortgageSimulator.py
+++ MortgageSimulator.py
@@ -2,8 +2,8 @@
 
 
 import streamlit as st
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+#import plotly.graph_objects as go
+#from plotly.subplots import make_subplots
 import numpy as np
 import numpy_financial as npf
 
